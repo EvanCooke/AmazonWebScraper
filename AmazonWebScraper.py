99045a611a04adc4f712471c7ae843bd54bd2b52
Evan Cooke <evcooke@uiowa.edu>
2022-02-20 21:37:07 -0600

    Prototyped record

--- AmazonWebScraper.py
+++ AmazonWebScraper.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 
 driver = webdriver.Chrome()
 
+
 # url = 'https://www.amazon.com'
 # driver.get(url)
 
@@ -13,7 +14,37 @@ def get_url(search_term):
     search_term = search_term.replace(' ', '+')
     return template.format(search_term)
 
-url = get_url('basketball')
-print(url)
+
+url = get_url('urltrawide monitor')
+# print(url)
 
 driver.get(url)
+
+# Extract the collection
+
+# soup object which retrieves html contents and uses parser
+# to parse it
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+# use soup object to extract all elements in html contents with data-component-type of s-search-result
+results = soup.find_all('div', {'data-component-type': 's-search-result'})
+print(len(results))
+
+# Prototype the record
+
+item = results[0]
+atag = item.h2.a
+description = atag.text.strip()
+url = 'https://www.amazon.com' + atag.get('href')
+
+price_parent = item.find('span', 'a-price')
+price = price_parent.find('span', 'a-offscreen').text
+
+# get i tag found from first item's html contents in results
+rating = item.i.text
+
+review_count = item.find('span', {'class': 'a-size-base', 'class': 's-underline-text'}).text
+
+print(review_count)
+
+
