dfc59fb37ccecd6e928844f3b793e2dc6b786eef
Evan Cooke <evcooke@uiowa.edu>
2022-02-20 19:35:49 -0600

    implemented get_url function

--- AmazonWebScraper.py
+++ AmazonWebScraper.py
@@ -4,5 +4,16 @@ from selenium import webdriver
 
 driver = webdriver.Chrome()
 
-url = 'https://www.amazon.com'
+# url = 'https://www.amazon.com'
+# driver.get(url)
+
+def get_url(search_term):
+    """Generate a URL from a search term"""
+    template = 'https://www.amazon.com/s?k={}&ref=nb_sb_noss'
+    search_term = search_term.replace(' ', '+')
+    return template.format(search_term)
+
+url = get_url('basketball')
+print(url)
+
 driver.get(url)
